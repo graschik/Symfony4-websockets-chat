ee94c0d1ec2575d9e8915385f1fc494fe804ad89
Alexandr Graschenko <allseeing98@gmail.com>
2018-05-02 16:02:31 +0300

    Signup temlate Version 1.0

---
+++ src/Service/SignupFormHandler.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allse
+ * Date: 02.05.2018
+ * Time: 15:19
+ */
+
+class SignupFormHandler
+{
+
+}
---
+++ src/Service/SignupService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: allse
+ * Date: 02.05.2018
+ * Time: 15:26
+ */
+
+namespace App\Service;
+
+
+class SignupService
+{
+
+}
