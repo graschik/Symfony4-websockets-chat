1f886a502815e4a70a8f56d5d93cbbb99fcdd35d
Alexandr Graschenko <allseeing98@gmail.com>
2018-05-24 22:05:35 +0300

    Added DoctrineReconnectHelper

--- src/Service/DoctrineReconnectHelper.php
+++ src/Service/DoctrineReconnectHelper.php
@@ -1,15 +1,65 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: allse
- * Date: 21.05.2018
- * Time: 21:37
- */
 
 namespace App\Service;
 
 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Debug\Debug;
+use Symfony\Component\Security\Core\Encoder\EncoderAwareInterface;
+
 class DoctrineReconnectHelper
 {
+    private $entityManager;
 
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    public function disconnect()
+    {
+        $this->entityManager->getConnection()->close();
+    }
+
+    public function connect()
+    {
+        $this->entityManager->getConnection()->connect();
+    }
+
+    /**
+     * MySQL Server has gone away
+     * @param EntityManagerInterface $entityManager
+     * @return EntityManagerInterface
+     */
+    public function reconnect(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $connection = $this->entityManager->getConnection();
+        if (!$connection->ping()) {
+
+            $this->disconnect();
+            $this->connect();
+
+            $this->checkEMConnection($connection);
+        }
+        return $this->entityManager;
+    }
+
+    /**
+     * method checks connection and reconnect if needed
+     * MySQL Server has gone away
+     *
+     * @param $connection
+     * @throws \Doctrine\ORM\ORMException
+     */
+    protected function checkEMConnection($connection)
+    {
+        if (!$this->entityManager->isOpen()) {
+            $config = $this->entityManager->getConfiguration();
+
+            $this->entityManager = $this->entityManager->create(
+                $connection, $config
+            );
+        }
+    }
 }
