cfd8b6528ed3bfb2813d45c1782ee9fbaaef5256
Alexandr Graschenko <allseeing98@gmail.com>
2018-05-30 23:43:39 +0300

    Added ajax for loading messages

---
+++ assets/js/ajax_message_search.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+    ajaxSearch();
+
+    function ajaxSearch() {
+        jQuery("#load").click(function () {
+            var outResult = "";
+            console.log('asdasd');
+            searchRequest = jQuery.ajax({
+                type: "GET",
+                url: "http://127.0.0.1:8000/ajax/front-controller",
+                data: {
+                    'route': 'ajax.message-search'
+                },
+                dataType: "text",
+                beforeSend: function () {
+                    var buttonWidth = $(".btn-load").css("width");
+                    $(".load-wrapp").css({
+                        'display': 'block'
+                    });
+                    $(".load-wrapp+span").css({
+                        'display': 'none'
+                    });
+                    $(".btn-load").css({
+                        'min-width': parseInt(buttonWidth)
+                    })
+                },
+                success: function (msg) {
+                    var result = JSON.parse(msg);
+                    $.each(result, function (index, value) {
+                        outResult += "<div class=\"panel panel-1\">\n" +
+                            "           <div class=\"row\">\n" +
+                            "             <div class=\"col-lg-12\">\n" +
+                            "                <div class=\"user\"><span class=\"name\">" + value.username + "</span><span class=\"date\">2018-05-23 23:37:43</span>\n" +
+                            "                </div>\n" +
+                            "             <div class=\"message\">" + value.message + "</div>\n" +
+                            "           </div>\n" +
+                            "         </div>\n" +
+                            "       </div>";
+                    });
+                    $("#status").prepend(outResult);
+                    $(".load-wrapp").css({
+                        'display': 'none'
+                    })
+                    $(".load-wrapp+span").css({
+                        'display': 'block'
+                    })
+                }
+            })
+        });
+    }
+
+})
